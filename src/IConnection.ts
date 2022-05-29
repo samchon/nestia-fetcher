f0ba09334cb761ebd5c52c3e2234ba253bc920bf
Jeongho Nam <samchon.github@gmail.com>
2022-05-29 21:10:52 +0900

    Close #6

--- src/IConnection.ts
+++ src/IConnection.ts
@@ -2,20 +2,19 @@ import { IEncryptionPassword } from "./IEncryptionPassword";
 
 /**
  * Connection information.
- * 
- * `IConnection` is a type of interface who represents connection information of the remote 
- * HTTP server. You can target the remote HTTP server by wring the {@link IConnection.host} 
- * variable down. Also, you can configure special header values by specializing the 
+ *
+ * `IConnection` is a type of interface who represents connection information of the remote
+ * HTTP server. You can target the remote HTTP server by wring the {@link IConnection.host}
+ * variable down. Also, you can configure special header values by specializing the
  * {@link IConnection.headers} variable.
- * 
- * If the remote HTTP server encrypts or decrypts its body data through the AES-128/256 
- * algorithm, specify the {@link IConnection.encryption} with {@link IEncryptionPassword} 
+ *
+ * If the remote HTTP server encrypts or decrypts its body data through the AES-128/256
+ * algorithm, specify the {@link IConnection.encryption} with {@link IEncryptionPassword}
  * or {@link IEncryptionPassword.Closure} variable.
- * 
+ *
  * @author Jenogho Nam - https://github.com/samchon
  */
-export interface IConnection
-{
+export interface IConnection {
     /**
      * Host address of the remote HTTP server.
      */
--- src/IEncryptionPassword.ts
+++ src/IEncryptionPassword.ts
@@ -1,15 +1,14 @@
 /**
  * Encryption password.
- * 
- * `IEncryptionPassword` is a type of interface who represents encryption password used by 
- * the {@link Fetcher} with AES-128/256 algorithm. If your encryption password is not fixed 
- * but changes according to the input content, you can utilize the 
+ *
+ * `IEncryptionPassword` is a type of interface who represents encryption password used by
+ * the {@link Fetcher} with AES-128/256 algorithm. If your encryption password is not fixed
+ * but changes according to the input content, you can utilize the
  * {@link IEncryptionPassword.Closure} function type.
- * 
+ *
  * @author Jeongho Nam - https://github.com/samchon
  */
-export interface IEncryptionPassword
-{
+export interface IEncryptionPassword {
     /**
      * Secret key.
      */
@@ -22,29 +21,32 @@ export interface IEncryptionPassword
 
     /**
      * Disable encryption to let content as plain.
-     * 
-     * When you configure this `disabled` variable to be `false`, encryption and decryption 
-     * algorithm would be disabled. Therefore, content like request or response body 
+     *
+     * When you configure this `disabled` variable to be `false`, encryption and decryption
+     * algorithm would be disabled. Therefore, content like request or response body
      * would be considered as a plain text instead.
-     * 
+     *
      * Default is `false`.
      */
-    disabled?: boolean | ((param: IEncryptionPassword.IParameter, encoded: boolean) => boolean);
+    disabled?:
+        | boolean
+        | ((
+              param: IEncryptionPassword.IParameter,
+              encoded: boolean,
+          ) => boolean);
 }
-export namespace IEncryptionPassword
-{
+export namespace IEncryptionPassword {
     /**
      * Type of a closure function returning the {@link IEncryptionPassword} object.
-     * 
+     *
      * `IEncryptionPassword.Closure` is a type of closure function who are returning the
      * {@link IEncryptionPassword} object. It would be used when your encryption password
      * be changed according to the input content.
      */
-    export interface Closure
-    {
+    export interface Closure {
         /**
          * Encryption password getter.
-         * 
+         *
          * @param param Request or response headers and body content
          * @param encoded Be encoded or to be decoded
          * @returns Encryption password
@@ -55,8 +57,7 @@ export namespace IEncryptionPassword
     /**
      * Parameter for the closure.
      */
-    export interface IParameter
-    {
+    export interface IParameter {
         headers: Record<string, string>;
         body: string;
     }
